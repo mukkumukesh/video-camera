90a5bbaf6bafe516d90e4bd2aa7b4908f93e62f3
Mukesh Yadav <rishabh.nagpal@jeeavnsathi.com>
2019-06-21 09:56:39 +0530

    Video camera sample application

---
+++ app/src/main/java/com/sample/videocamerasample/VideoCameraActivity.kt
@@ -0,0 +1,12 @@
+package com.sample.videocamerasample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class VideoCameraActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_video_camera)
+    }
+}
